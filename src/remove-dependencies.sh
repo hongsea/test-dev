527a95d080473d3c3153e6070d8afc49b7137753
hongsea <hongsea119@gmail.com>
2022-09-01 16:13:44 +0700

    update

--- src/remove-dependencies.sh
+++ src/remove-dependencies.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 # set -x
-npx depcheck .. | sed '/Missing/q' | grep '\*' | cut -c 3- > file.txt
+npx depcheck | sed '/Missing/q' | grep '\*' | cut -c 3- > file.txt
 count=$(wc -l file.txt | awk '{print $1}')
+
 echo "Checked Unued Dependencies: $count unued"
 
-npx depcheck .. | sed '/Missing/q' | grep '\*' | cut -c 3- | while read my_package; do
+npx depcheck | sed '/Missing/q' | grep '\*' | cut -c 3- | while read my_package; do
   echo "$my_package"
 done;
